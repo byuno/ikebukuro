22949de7a6b5c5a44ef606ea4637595feb19cbbc
byuno <robert.y.uno@gmail.com>
2024-12-03 15:33:32 -0800

    find station test case

---
+++ tests/Find-station.spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@playwright/test';
+
+test('Verify deals - Vancouver', async ({ page }) => {
+    await page.goto('https://journie.ca/en-CA');
+  
+    //Click 
+    await page.locator('#Header').getByText('Deals').nth(2).click();
+
+    //Click first deal
+    await page.locator('[class="col-xs-12 col-sm-4 col-md-4 promo-margin"]').first().click()
+
+    //Click the Find a station button
+    await page.getByRole('button', {name:'Find a station'}).click()
+    
+    //Click on search bar and fill 
+    await page.getByPlaceholder('Enter a location').fill('v5s4r2');
+
+    //Select Vancouver
+    await page.getByText('V5S 4R2Vancouver, BC, Canada').click();
+    
+    //
+    await page.getByText('We could not find any').isVisible();
+
+  });
+  
